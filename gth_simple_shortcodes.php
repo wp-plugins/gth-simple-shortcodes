49cd5d216d29a785f4fc3d57e925190f63ba5287
paul-at-greentiehosting <paul-at-greentiehosting@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-21 15:24:34 +0000

    initial upload
    git-svn-id: https://plugins.svn.wordpress.org/gth-simple-shortcodes/trunk/gth_simple_shortcodes@1072643 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gth_simple_shortcodes.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Plugin Name: Simple Shortcode Master
+ * Plugin URI: http://www.greentiehosting.com/simple-shortcodes
+ * Description: A extremely lightweight plugin that gives you a massive amount of shortcodes at the ready.
+ * Version: 1.0.0.1
+ * Author: Green Tie Hosting
+ * Author URI: http://www.greentiehosting.com
+ * Text Domain: gthss
+ * License: GPL2
+ */
+ // Kill script if accessed directly.
+defined('ABSPATH') or die("No script kiddies please!");
+ 
+ /** Website Information **/
+ // Add Website Name shortcode [gthss_site_name]
+function gthss_sitename(){
+   return get_bloginfo();
+}
+add_shortcode( 'gthss_site_name', 'gthss_sitename' );
+ // Add Website tagline shortcode [gthss_site_tagline]
+function gthss_sitetagline(){
+   return get_bloginfo('description');
+}
+add_shortcode( 'gthss_site_tagline', 'gthss_sitetagline' );
+  // Add Website URL shortcode [gthss_root_url]
+function gthss_root_url() {
+	return site_url();
+}
+add_shortcode( 'gthss_root_url', 'gthss_root_url');
+  // Add Website Admin URL shortcode [gthss_admin_url]
+function gthss_admin_url() {
+	return site_url();
+}
+add_shortcode( 'gthss_admin_url', 'gthss_admin_url');
+
+ /** Page/Post Information **/
+ // Add Page_title shortcode [gthss_page_title]
+function gthss_pagetitle(){
+   return get_the_title();
+}
+add_shortcode( 'gthss_page_title', 'gthss_pagetitle' );
+// Add Page permalink shortcode [gthss_permalink]
+function gthss_permalink() {
+	return get_permalink();
+}
+add_shortcode( 'gthss_permalink', 'gthss_permalink');
+
+/** User Information **/
+// Add current username shortcode [gthss_username]
+function gthss_username() {
+	if(!is_user_logged_in()) {
+		return 'Guest';
+	}
+	else{
+		global $current_user;
+		get_currentuserinfo();
+		return $current_user->user_login;
+	}
+}
+add_shortcode( 'gthss_username', 'gthss_username');
+// Add current display name shortcode [gthss_displayname]
+function gthss_displayname() {
+	if(!is_user_logged_in()) {
+		return 'Guest';
+	}
+	else{
+		global $user_identity;
+		get_currentuserinfo();
+		return $user_identity;
+	}
+}
+add_shortcode( 'gthss_displayname', 'gthss_displayname');
+
+ /** General Wordpress Extras **/
+ // Add Search Form shortcode [gthss_search]
+ function gthss_search() {
+	return get_search_form();
+ }
+add_shortcode( 'gthss_search', 'gthss_search');
+
+ /** General PHP extras **/
+ // Add current month shortcode [gthss_curr_month]
+function gthss_currentmonth() {
+	return date('M');
+}
+add_shortcode( 'gthss_curr_month', 'gthss_currentmonth');
+ // Add current year shortcode [gthss_curr_year]
+function gthss_currentyear() {
+	return date('Y');
+}
+add_shortcode( 'gthss_curr_year', 'gthss_currentyear');
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Plugin Name ===
+Contributors: paul-at-greentiehosting
+Donate link: http://www.greentiehosting.com/
+Tags: shortcodes, php functions
+Requires at least: 3.0.1
+Tested up to: 4.1
+Stable tag: 1.0.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A extremely lightweight plugin that gives you the most popular shortcodes at the ready.
+
+== Description ==
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `gth_ssm.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place needed shortcodes in your website content
+
+== Frequently Asked Questions ==
+
+= What are the available shortcodes? =
+
+[gthss_site_name] - Displays your website's Name
+[gthss_site_tagline] - Displays your website's tagline
+[gthss_root_url] - Displays your website's root URL without the trailing slash. ie. http://www.wordpress.org
+[gthss_admin_url] - Displays your website's admin URL without the trailing slash. ie. http://www.wordpress.org/wp-admin
+[gthss_page_title] - Displays the current page's/post's title
+[gthss_permalink] - Displays your current page's/post's permalink
+[gthss_username] - Displays the current logged in user's username. If they are not logged in it will display "Guest".
+[gthss_displayname] - Displays the cuurrent loggedin user's display name. If they are not logged in it will display "Guest".
+[gthss_curr_year] - Displays the current year. ie. 2015
+[gthss_curr_month] - Displays the current month. ie. Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
+
+= You don't have ... Can you add it? =
+
+Please submit a request for an additional shortcode to be added on our site: https://www.greentiehosting.com/simple-shortcodes/
+
+== Changelog ==
+
+= 1.0.0.1 =
+ * Initial Launch Version with 10 shortcodes
+ 
